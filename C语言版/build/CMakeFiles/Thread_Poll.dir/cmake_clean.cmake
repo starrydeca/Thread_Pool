b9e47013637f834bc9d2a8fbdb3d0d220cda7537
StarsDecade <3519506439@qq.com>
2023-07-27 10:18:22 +0800

    CMake 编译

---
+++ C语言版/build/CMakeFiles/Thread_Poll.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/starsdecade/文档/Thread_Pool/C语言版/Thread_Poll"
+  "/home/starsdecade/文档/Thread_Pool/C语言版/Thread_Poll.pdb"
+  "CMakeFiles/Thread_Poll.dir/src/Thread_Pool.c.o"
+  "CMakeFiles/Thread_Poll.dir/src/Thread_Pool.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Thread_Poll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
